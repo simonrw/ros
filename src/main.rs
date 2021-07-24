4a2607f9baa976a453e50436fdb7749a74a10a6b
Simon Walker <s.r.walker101@googlemail.com>
2021-07-24 21:30:34 +0100

    Adds interrupt handler

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,10 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("ROS starting");
 
+    ros::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
